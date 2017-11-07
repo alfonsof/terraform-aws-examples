d9aa97eec357dd1bf0ac02aa74a48eb1d263ee45
Fernandez Barandiaran <VT88SE@ad.ing.net>
2017-11-07 12:10:30 +0100

    Added variable server_port and output public_ip

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = "8080"
+}
+
 provider "aws" {
   region = "eu-west-1"
 }
@@ -6,8 +11,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   
   ingress {
-    from_port	= 8080
-	to_port		= 8080
+    from_port	= "${var.server_port}"
+	to_port		= "${var.server_port}"
 	protocol	= "tcp"
 	cidr_blocks	= ["0.0.0.0/0"]
   }
@@ -21,11 +26,15 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
 			  #!/bin/bash
 			  echo "Hello, World" > index.html
-			  nohup busybox httpd -f -p 8080 &
+			  nohup busybox httpd -f -p "${var.server_port}" &
 			  EOF
 			  
   tags {
     Name = "terraform-example"
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
 
