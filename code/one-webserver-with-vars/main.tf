320f196ff494e5fdd8a277c45c395ec93d346074
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-11-07 12:50:48 +0100

    Create main.tf
    Create a Web Server with Busybox in Ubuntu using vars
    input variable: server_port
    output variable: public_ip

---
+++ code/one-webserver-with-vars/main.tf
@@ -0,0 +1,40 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = "8080"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+ 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port	= "${var.server_port}"
+	to_port		= "${var.server_port}"
+	protocol	= "tcp"
+	cidr_blocks	= ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami			= "ami-785db401"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  
+  user_data = <<-EOF
+			  #!/bin/bash
+			  echo "Hello, World" > index.html
+			  nohup busybox httpd -f -p "${var.server_port}" &
+			  EOF
+			  
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
+
