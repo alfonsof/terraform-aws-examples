6d878f7cf24ee97a3d90f4b2c59b5df773ff5e53
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-11-07 12:53:24 +0100

    Update main.tf
    tabs

--- code/one-webserver/main.tf
+++ code/one-webserver/main.tf
@@ -7,22 +7,22 @@ resource "aws_security_group" "instance" {
   
   ingress {
     from_port	= 8080
-	to_port		= 8080
-	protocol	= "tcp"
-	cidr_blocks	= ["0.0.0.0/0"]
+    to_port	= 8080
+    protocol	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "example" {
-  ami			= "ami-785db401"
+  ami = "ami-785db401"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   
   user_data = <<-EOF
-			  #!/bin/bash
-			  echo "Hello, World" > index.html
-			  nohup busybox httpd -f -p 8080 &
-			  EOF
+	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p 8080 &
+	      EOF
 			  
   tags {
     Name = "terraform-example"
