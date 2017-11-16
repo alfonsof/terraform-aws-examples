45857e65b9f68379e8ff5652140fd8ebf2a311d2
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-11-16 10:52:47 +0100

    Add vars for the name of the S3 bucket and key where the remote state

--- code/08-file-layout-example/stage/services/webserver-cluster/vars.tf
+++ code/08-file-layout-example/stage/services/webserver-cluster/vars.tf
@@ -2,3 +2,13 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default = "8080"
 }
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  default     =  "terraform-state-afb"
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  default     = "file-layout-example/stage/data-stores/mysql/terraform.tfstate"
+}
