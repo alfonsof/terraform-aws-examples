fc2dc0359a3d1ffbc2779e3235b212799e56e55d
Alfonso Fernandez-Barandiaran <alfonsofb@gmail.com>
2017-11-17 12:26:15 +0100

    Create main.tf

---
+++ code/09-module-example/prod/data-stores/mysql/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_db_instance" "example" {
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  name                = "example_database_prod"
+  username            = "admin"
+  password            = "${var.db_password}"
+  skip_final_snapshot = true
+}
